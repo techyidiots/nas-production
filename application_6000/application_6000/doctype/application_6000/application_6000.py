4c5a87120320e3a5d60366b33b45aab003f7fb74
Syed Abdul Qadeer <sdqadeer44@gmail.com>
2017-11-21 15:17:24 +0300

    [Added]- the support for notifications

--- application_6000/application_6000/doctype/application_6000/application_6000.py
+++ application_6000/application_6000/doctype/application_6000/application_6000.py
@@ -5,6 +5,71 @@
 from __future__ import unicode_literals
 import frappe
 from frappe.model.document import Document
+from frappe.utils.user import get_system_managers
+from frappe.translate import get_user_lang
+from frappe import _
 
 class Application6000(Document):
-	pass
+	def before_insert(self):
+		self.flags.in_insert = True
+
+	def on_update(self):
+		users = get_system_managers(True)
+
+		if self.flags.in_insert:
+
+			if users:
+				for user in users:
+					user_lang = get_user_lang(user)
+					email_subject = _("A New Application : %(name)s is created", user_lang)%{
+				"name": self.name,
+				"status": self.status
+			}
+					email_content = _(""" 
+					A new application is created
+					""", user_lang)%{
+				"name": self.name,
+				"status": self.status
+			}
+
+					frappe.sendmail(recipients=user, subject=email_subject, content=email_content)
+
+			if self.email:
+				email_subject = _("We have received your Application : " + self.name)
+				email_content = _("""
+					Your application was received!
+				""") % {
+					"name": self.name,
+					"status": self.status
+				}
+
+				frappe.sendmail(recipients=self.email, subject=email_subject, content=email_content)
+
+			return
+
+
+		if users:
+			for user in users:
+				user_lang = get_user_lang(user)
+				email_subject = _(self.name + " have new updates", user_lang)
+				email_content = _(""" 
+				%(name)s status was changed to : %(status)s
+				""", user_lang)%{
+				"name": self.name,
+				"status": self.status
+			}
+
+				frappe.sendmail(recipients=user, subject=email_subject, content=email_content)
+
+		if self.email:
+			email_subject = _(self.name + " have new updates")
+			email_content = _("""
+				%(name)s status was changed to : %(status)s
+			""")%{
+				"name": self.name,
+				"status": self.status
+			}
+
+			frappe.sendmail(recipients=self.email, subject=email_subject, content=email_content)
+
+		return
--- application_6000/application_6000/doctype/application_6000_task/application_6000_task.py
+++ application_6000/application_6000/doctype/application_6000_task/application_6000_task.py
@@ -5,6 +5,37 @@
 from __future__ import unicode_literals
 import frappe
 from frappe.model.document import Document
+from frappe.translate import get_user_lang
+from frappe import _
 
 class Application6000Task(Document):
+	def before_insert(self):
+		self.flags.in_insert = True
+
+	def on_update(self):
+		if self.flags.in_insert:
+			if self.user:
+				user_lang = get_user_lang(self.user)
+				email_subject = _(self.name + " was assigned to you")
+				email_content = _("""
+				    		%(name)s status was changed to : %(status)s
+				    	""", user_lang) % {
+					"name": self.name,
+					"status": self.status
+				}
+
+				frappe.sendmail(recipients=self.user, subject=email_subject, content=email_content)
+			return
+
+		if not self.user == self.modified_by:
+			user_lang = get_user_lang(self.user)
+			email_subject = _(self.name + " have new updates")
+			email_content = _("""
+	    		%(name)s status was changed to : %(status)s
+	    	""", user_lang) % {
+				"name": self.name,
+				"status": self.status
+			}
+
+			frappe.sendmail(recipients=self.user, subject=email_subject, content=email_content)
 	pass
