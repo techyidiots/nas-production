9f3c2ed0cd8e04fdcdd4a9d3bb01cf433dce930d
Syed Abdul Qadeer <sdqadeer44@gmail.com>
2017-11-22 15:10:22 +0300

    [Added] - the support for uri.js

--- application_6000/public/js/web.js
+++ application_6000/public/js/web.js
@@ -18,4 +18,58 @@ $(document).ready(function(e){
 
        window.location = new_url;
     });
-})
+
+    $("a").each(function(){
+
+    });
+});
+
+frappe.urllib = {
+
+	// get argument from url
+	get_arg: function(url, name) {
+		name = name.replace(/[\[]/,"\\\[").replace(/[\]]/,"\\\]");
+		var regexS = "[\\?&]"+name+"=([^&#]*)";
+		var regex = new RegExp( regexS );
+		var results = regex.exec( url || window.location.href );
+		if( results == null )
+			return "";
+		else
+			return decodeURIComponent(results[1]);
+	},
+
+	// returns url dictionary
+	get_dict: function(url) {
+		var d = {}
+		var t = url || window.location.href.split('?')[1];
+		if(!t) return d;
+
+		if(t.indexOf('#')!=-1) t = t.split('#')[0];
+		if(!t) return d;
+
+		t = t.split('&');
+		for(var i=0; i<t.length; i++) {
+			var a = t[i].split('=');
+			d[decodeURIComponent(a[0])] = decodeURIComponent(a[1]);
+		}
+		return d;
+	},
+
+	// returns the base url with http + domain + path (-index.cgi or # or ?)
+	get_base_url: function(url) {
+		// var url= (frappe.base_url || window.location.href).split('#')[0].split('?')[0].split('desk')[0];
+		url = (url || window.location.origin)
+		if(url.substr(url.length-1, 1)=='/') url = url.substr(0, url.length-1)
+		return url
+	},
+
+	// returns absolute url
+	get_full_url: function(url) {
+		if(url.indexOf("http://")===0 || url.indexOf("https://")===0) {
+			return url;
+		}
+		return url.substr(0,1)==="/" ?
+			(frappe.urllib.get_base_url() + url) :
+			(frappe.urllib.get_base_url() + "/" + url);
+	}
+}
