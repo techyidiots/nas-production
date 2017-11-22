d3fefe378cc12225440fa216b07f12cd0b723201
Syed Abdul Qadeer <sdqadeer44@gmail.com>
2017-11-22 12:06:54 +0300

    [Added] - the support for context

---
+++ application_6000/www/index.py
@@ -0,0 +1,11 @@
+# Copyright (c) 2015, Frappe Technologies Pvt. Ltd. and Contributors
+# MIT License. See license.txt
+
+from __future__ import unicode_literals
+
+import frappe
+from frappe.utils import now
+from frappe import _
+
+def get_context(context):
+	context.main_section = "Coolest"
