4eb9eb3a6e854f906428d1faaa044e92d284ff47
Syed Abdul Qadeer <sdqadeer44@gmail.com>
2017-11-27 01:37:54 +0300

    [Testing] - the  click event of the dropdown

--- application_6000/public/js/web.js
+++ application_6000/public/js/web.js
@@ -1,7 +1,47 @@
 $(document).ready(function(e){
-    
+    var set_language = function(language){
+       var query_params = frappe.utils.get_query_params();
+       query_params["_lang"] = language;
+
+       var query_strings = frappe.utils.make_query_string(query_params);
+
+       var loc = window.location;
+
+       var new_url = loc.protocol + "//" + loc.hostname + (loc.port ? ":"+loc.port : "") +
+           loc.pathname + (query_strings && query_strings.trim()!=="?" ? query_strings : "") + loc.hash;
+
+       window.location = new_url;
+    }
+
+    $("li[data-label=English]").find("a").on("click", function(e){
+        e.preventDefault();
+
+       var language = "ar";
+       if($(this).parent("li").attr("data-label") == "English"){
+           language = "en"
+       }
+
+       set_language(language);
+    });
+
+    $("li[data-label=Arabic]").find("a").on("click", function(e){
+       e.preventDefault();
+
+       var language = "ar";
+       if($(this).parent("li").attr("data-label") == "English"){
+           language = "en"
+       }
+
+       set_language(language);
+    });
+
 
     var query_params = frappe.utils.get_query_params();
+
+    if(query_params && query_params["_lang"]==="en")
+        $("li[data-label=English]").hide();
+    else
+        $("li[data-label=Arabic]").hide()
 
     $("a").each(function(){
         if(!query_params["_lang"])
