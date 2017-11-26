93290c5179fa70e8af28dbf6e1fadf494e528b88
Syed Abdul Qadeer <sdqadeer44@gmail.com>
2017-11-27 01:13:09 +0300

    [Testing] - the  click event of the dropdown

--- application_6000/public/js/web.js
+++ application_6000/public/js/web.js
@@ -1,28 +1,5 @@
 $(document).ready(function(e){
-    $("li[data-label=Language]").find("a").on("click", function(e){
-        e.preventDefault();
-
-        if($(this).parent("li").attr("data-label")==="Language"){
-            return;
-        }
-
-        var language = "ar";
-       if($(this).parent("li").attr("data-label") == "English"){
-           language = "en"
-       }
-
-       var query_params = frappe.utils.get_query_params();
-       query_params["_lang"] = language;
-
-       var query_strings = frappe.utils.make_query_string(query_params);
-
-       var loc = window.location;
-
-       var new_url = loc.protocol + "//" + loc.hostname + (loc.port ? ":"+loc.port : "") +
-           loc.pathname + (query_strings && query_strings.trim()!=="?" ? query_strings : "") + loc.hash;
-
-       window.location = new_url;
-    });
+    
 
     var query_params = frappe.utils.get_query_params();
 
