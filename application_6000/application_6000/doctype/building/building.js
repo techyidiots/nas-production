09c16075066c2169c7f013df40faf829f6dfd6ca
Syed Abdul Qadeer <sdqadeer44@gmail.com>
2017-12-06 10:04:06 +0000

    [Added] - the fixtures and the building doctype

---
+++ application_6000/application_6000/doctype/building/building.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2017, Syed Abdul Qadeer and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Building', {
+	refresh: function(frm) {
+
+	}
+});
